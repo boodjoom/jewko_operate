ddcea903836e8a0c5d843e50cd2b4668c9f454fb
Vladimir Shark <shark@toughshark.(none)>
2013-11-05 23:53:32 +0200

    first versions added

---
+++ jewko_joy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+killall player
+killall playercam
+killall python
+sleep 2
+#killall playerv
+#killall irobex_controll
+player jewko_ctrl.cfg&
+#playercam -h 192.168.88.188 -i 0&
+#playercam -h 192.168.59.188 -i 4&
+#python /home/shark/robotics/ros/voice_command/voice_cmd_player.py
+#/home/shark/QT_project/irobex_controll-build-desktop-Qt_4_8_1___PATH__________________/irobex_controll&
+#playerv -h 192.168.59.188
+sleep 2
+./jewko_control&
+./irobex_controll&
+
